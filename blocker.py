5067bb4e33010708049da3a9e98be06d3242056c
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2024-01-11 23:51:36 +0100

    feat: update

--- blocker.py
+++ blocker.py
@@ -6,18 +6,19 @@ from datetime import datetime as dt
 from ip_addresses import ip_addresses
 from web_to_be_blocked import web_list
 
-host_temp = "hosts"
+host_temp = "/etc/hosts"
 # redirect = ip_addresses
 
 while True:
-    if (dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 23)):
+    if (dt(dt.now().year, dt.now().month, dt.now().day, 8, 59) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 5, 59)):
         print("Working Hours....")
         with open(host_temp, mode='r+', encoding='utf-8') as f:
             content = f.read()
             # print(content)
             for website in web_list:
                 if website in content:
-                    print(content)
+                    # print(content)
+                    pass
                 else:
                     for ip_address in ip_addresses:
                         # redirect = ip_address
