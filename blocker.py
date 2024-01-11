54a16a1e5e6ede97b81cd0c204ffa5656ba51fdb
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2024-01-12 00:59:52 +0100

    feat: update with logic. It works on both Unix and Windows system

--- blocker.py
+++ blocker.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 import time
-import platform
+from platform import system as sys
 from datetime import datetime as dt
 
 from ip_addresses import ip_addresses
 from web_to_be_blocked import web_list
 
-host_temp = "/etc/hosts"
+if sys == "Windows":
+    host_temp = r"C:\Windows\System32\drivers\etc\hosts"
 # redirect = ip_addresses
-
+else:
+    host_temp = "/etc/hosts"
 while True:
     if (dt(dt.now().year, dt.now().month, dt.now().day, 8, 59) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 5, 59)):
-        print("Working Hours....")
+        print("Working hours...")
         with open(host_temp, mode='r+', encoding='utf-8') as f:
             content = f.read()
             # print(content)
